680770a6770a37ff76603c799eb22f65790f9d18
Luis Candanedo <luismiguel_78@hotmail.com>
2024-09-04 11:05:02 -0700

    updating code

--- day1_arithoper.c
+++ day1_arithoper.c
@@ -6,10 +6,10 @@ int main(){
  int c = 5;
  int d = 7;
  printf("a %d + b %d = %d \n",a,b,a+b);
- printf("modulo operation a %% b = %d, a is %d and be is %d \n",a%b,a,b);
- printf("modulo operation b %% a = %d, a is %d and be is %d \n",b%a,b,a);
- printf("modulo operation a %% c = %d, a is %d and c  is %d \n",a%c,a,c);
- printf("modulo operation a %% d = %d, a is %d and d  is %d \n",a%d,a,d);
+ printf("modulo operation a %% b = %d, a is %d and be is %d \n",a%b,a,b);// 10%20= 10
+ printf("modulo operation b %% a = %d, a is %d and be is %d \n",b%a,b,a);//20%10 = 0
+ printf("modulo operation a %% c = %d, a is %d and c  is %d \n",a%c,a,c);//10%5 = 0
+ printf("modulo operation a %% d = %d, a is %d and d  is %d \n",a%d,a,d);//10%7  = 3
  return 0;
 
 }
